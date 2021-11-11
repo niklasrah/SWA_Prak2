92d20ddfc562491b106f90a3a848c1f4f3b26369
niklasrah <n.rah@web.de>
2021-11-11 15:09:42 +0100

    main fix

--- GenerischerRingpuffer1/src/main/java/com/example/App.java
+++ GenerischerRingpuffer1/src/main/java/com/example/App.java
@@ -1,18 +1,10 @@
 package com.example;
 
-/**
- * Hello world!
- */
 public final class App {
-    private App() {
-    }
-
     /**
-     * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         Ringpuffer<Integer> ring = new Ringpuffer<Integer>();
         ring.add(5);
         ring.add(6);
