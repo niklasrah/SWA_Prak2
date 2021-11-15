fd1f1f7ab5c83bfb86c65aed1650c4dcb20a4caf
niklasrah <n.rah@web.de>
2021-11-15 12:54:09 +0100

    Kommentare

--- GenerischerRingpuffer1/src/main/java/messaging/App.java
+++ GenerischerRingpuffer1/src/main/java/messaging/App.java
@@ -8,18 +8,23 @@ public final class App {
         Producer<Integer> pro = new Producer<Integer>();
         Consumer<Integer> con1 = new Consumer<Integer>("Niklas");
         Consumer<Integer> con2 = new Consumer<Integer>("Steffen");
+
         pro.addListener(con1, "GanzeZahlen");
         pro.addListener(con2, "Primzahlen");
-        pro.addMessage("GanzeZahlen", 0);
+
+        pro.addMessage("GanzeZahlen", 0); // Erweiterbare MessageQueue GanzeZahlen
         pro.addMessage("GanzeZahlen", 1);
-        pro.addMessage("Primzahlen", 2, true, false);
+
+        pro.addMessage("Primzahlen", 2, true, false); // MessageQueue Primzahlen mit fester Groeße
         pro.addMessage("Primzahlen", 3);
         pro.addMessage("Primzahlen", 5);
         pro.addMessage("Primzahlen", 7);
-        pro.addMessage("Primzahlen", 11);
+        pro.addMessage("Primzahlen", 11); // MessageQueue ist voll!
+
         con1.getMessage("GanzeZahlen");
         con1.getMessage("GanzeZahlen");
-        con1.getMessage("GanzeZahlen");
-        con1.getMessage("NatuerlicheZahlen");
+        con1.getMessage("GanzeZahlen"); // MessageQueue ist leer
+
+        con1.getMessage("NatuerlicheZahlen"); // MessageQueue existiert garnicht
     }
 }
